c0090175ac60a7c0153392619d38ca8da821f0f5
bamtests <17178592+bamtests@users.noreply.github.com>
2023-10-28 17:07:04 -0400

    Update custom.config.php

--- custom.config.php
+++ custom.config.php
@@ -2,6 +2,7 @@
 $CONFIG = array(
     'htaccess.RewriteBase' => '/',
     'overwritewebroot' => '/',
-    'default_phone_region' => 'US',
+    'overwriteprotocol' => 'https'
+    'default_phone_region' => 'us',
     'forwarded_for_headers' => ['HTTP_X_FORWARDED', 'HTTP_FORWARDED_FOR'],
 );
